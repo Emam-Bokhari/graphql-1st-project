5f53f4e5e1e3dc18c2a8558491cbb8dce822f4e6
Moshfiqur Rahman <studentemam@gmail.com>
2025-10-29 10:29:20 +0600

    feat: add getBlogsFromDB service

--- src/app/modules/Blog/blog.service.ts
+++ src/app/modules/Blog/blog.service.ts
@@ -1,16 +1,27 @@
 import ApiError from "../../errors/ApiError";
+import { TBlog } from "./blog.interface";
 import { Blog } from "./blog.model"
 
-const createBlogToDB = async () => {
+const createBlogToDB = async (payload: TBlog) => {
+    const blog = await Blog.create(payload);
+    if (!blog) {
+        throw new ApiError(400, "Failed to create blog")
+    };
+    return blog;
+}
+
+const getBlogsFromDB = async () => {
     const blogs = await Blog.find();
+
     if (!blogs || blogs.length === 0) {
         throw new ApiError(404, "No blogs are found in the database")
     };
+
     return blogs;
+
 }
-
-
 
 export const BlogServices = {
     createBlogToDB,
+    getBlogsFromDB,
 }
