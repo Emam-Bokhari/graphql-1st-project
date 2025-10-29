2af09ee6f948302952047ef783dc99a66f4d2f75
Moshfiqur Rahman <studentemam@gmail.com>
2025-10-29 10:18:48 +0600

    create blogSchema

---
+++ src/app/modules/Blog/blog.model.ts
@@ -0,0 +1,27 @@
+import { model, Schema } from "mongoose";
+import { TBlog } from "./blog.interface";
+
+const blogSchema = new Schema<TBlog>({
+    title: {
+        type: String,
+        required: true,
+        min: 6
+    },
+    content: {
+        type: String,
+        required: true,
+        min: 100,
+    },
+    author: {
+        type: String,
+        required: true,
+        min: 6
+    }
+},
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+);
+
+export const Blog = model("Blog", blogSchema)
