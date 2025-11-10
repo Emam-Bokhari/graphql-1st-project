cd8d01906af1db0bcfa1458c717c8495d104ee5b
Moshfiqur Rahman <studentemam@gmail.com>
2025-11-10 11:48:59 +0600

    feat: add productSchema mongoose schema

---
+++ src/app/modules/Product/product.model.ts
@@ -0,0 +1,24 @@
+import { model, Schema } from "mongoose";
+import { TProduct } from "./product.interface";
+
+const productSchema = new Schema<TProduct>({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    }
+},
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+);
+
+export const Product = model("Product", productSchema)
